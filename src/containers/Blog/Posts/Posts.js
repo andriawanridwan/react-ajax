b02abe7078ba0e33c533d34826b1fdcc9324e852
andriawanridwan <andriridwan787@gmail.com>
2020-08-05 09:39:16 +0700

    React Ajax

---
+++ src/containers/Blog/Posts/Posts.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import axios from '../../../axios';
+import Post from '../../../components/Post/Post';
+import './Posts.css';
+import FullPost from '../FullPost/FullPost';
+import { Route } from 'react-router-dom';
+class Posts extends Component {
+    state = {
+        posts: [],
+    }
+    componentDidMount() {
+        console.log(this.props);
+        axios.get('/posts')
+            .then(response => {
+                const posts = response.data.slice(0, 4);
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Andriawan'
+                    }
+                });
+                this.setState({ posts: updatedPosts });
+            })
+
+    }
+    postSelectedId = (id) => {
+        this.props.history.push({ pathname: '/posts/' + id });
+    }
+    render() {
+        const posts = this.state.posts.map(post => {
+            return (
+                // <Link to={'/' + post.id} key={post.id}>
+                <Post
+                    key={post.id}
+                    title={post.title}
+                    author={post.author}
+                    clicked={() => this.postSelectedId(post.id)}
+                />
+            )
+            // </Link>) 
+        });
+        return (
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <Route path={this.props.match.url + "/:id"} exact component={FullPost} />
+            </div>
+
+        )
+    }
+}
+
+export default Posts;
